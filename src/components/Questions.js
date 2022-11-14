17defe3c0e812949f10f5c9c2be59a5fcab3d290
JoonaTeuho <joona.teuho@gmail.com>
2022-11-14 12:05:15 +0200

    changed port

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,7 +4,7 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 
-export default function Questions({showQuestions, params}){
+export default function Questions(){
   
     const [questions, setQuestions] = React.useState([])
     const [id, setId] = React.useState('')
@@ -22,7 +22,7 @@ export default function Questions({showQuestions, params}){
     }
 
     const fetchQuestions = () => {
-        fetch(`https://quizservicebackend.herokuapp.com/quizlist`)
+        fetch('https://quizservicebackend.herokuapp.com/quizlist')
         .then(response => response.json())
         .then(data => setQuestions(data[id].questions))
     }
--- src/index.js
+++ src/index.js
@@ -15,3 +15,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+App.listen(process.env.PORT || 3000);
